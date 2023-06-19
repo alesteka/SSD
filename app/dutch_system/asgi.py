2c2cad2c24d24a1e8d733959abfd3f87cdd15497
alesteka <tekavcicales1@gmail.com>
2023-06-19 20:20:26 +0200

    working dockerfile

--- app/dutch_system/asgi.py
+++ app/dutch_system/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dutchSystem project.
+ASGI config for dutch_system project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dutchSystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dutch_system.settings')
 
 application = get_asgi_application()
--- app/dutch_system/wsgi.py
+++ app/dutch_system/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for dutchSystem project.
+WSGI config for dutch_system project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dutchSystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dutch_system.settings')
 
 application = get_wsgi_application()
